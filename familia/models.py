587602ba0e0ec81449bad634b7a1778b35f8de74
esthorace <esteban.horacio.acevedo@gmail.com>
2022-05-27 12:38:30 -0300

    correcciones

--- familia/models.py
+++ familia/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class Persona(models.Model):
     nombre = models.TextField(max_length=100)
     altura = models.FloatField()
     fecha_nacimiento = models.DateField()
-    email = models.TextField(default="")
+    email = models.EmailField()
 
     def calcular_edad(self):
-        return 2022 - self.fecha_nacimiento.year
+        import datetime
+
+        año_hoy = datetime.date.today().year
+        return año_hoy - self.fecha_nacimiento.year
--- familia/views.py
+++ familia/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-from familia.models import *
-from django.template import loader
+from familia.models import Persona
+
 
 def hola_mundo(request):
     return HttpResponse("Hola Mundo")
 
-def plantilla(request):
-    Personas = Persona.objects.all()
-    nahuel = Persona.objects.first()
-    return HttpResponse(render(request, "familia/index.html", {"about" : nahuel}))
 
+def plantilla(request):
+    consulta = Persona.objects.all()
+    return HttpResponse(render(request, "familia/index.html", {"personas": consulta}))
